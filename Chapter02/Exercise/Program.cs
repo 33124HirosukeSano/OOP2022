003fe4970f0852b122e498376f609b6d5a05275b
33124佐野仁亮 <33124@ojs.ac.jp>
2022-05-10 15:57:49 +0900

    2.1.4

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -8,8 +8,63 @@ namespace Exercise {
     class Program {
         static void Main(string[] args) {
 
-            
+            /*List<Song> songs = new List<Song>();
+
+            var song1 = new Song("Let it be","Beatles",243);
+            songs.Add(song1);
+
+            var song2 = new Song("Let it be", "Beatles", 243);
+            songs.Add(song2);
+
+            var song3 = new Song("Let it be", "Beatles", 243);
+            songs.Add(song3);*/
+
+            var songs = new Song[] {
+
+                new Song("Let it be","Beatles",243),
+                new Song("Bridge Over Troubled","Simon & Garfunkel",293),
+                new Song("a","Aa",286),
+                new Song("b","Bb",224),
+                new Song("c","Cc",275),
+
+
+            };
+            PrintSongs(songs);
 
         }
+
+        /*private static void PrintSongs(Song[] songs) {
+
+            foreach (var song in songs) {
+
+                Console.WriteLine("{0} {1} {2:mm\\:ss}",
+                    song.Title, song.ArtistName, TimeSpan.FromSeconds(song.Length));
+
+
+            }
+        }
+        private static void PrintSong(List<Song> songs) {
+
+            foreach (var song in songs) {
+
+                Console.WriteLine("{0} {1} {2:mm\\:ss}",
+                    song.Title, song.ArtistName, TimeSpan.FromSeconds(song.Length));
+
+
+            }
+        }*/
+
+
+            private static void PrintSong(IEnumerable<Song> songs) {
+
+            foreach (var song in songs) {
+
+                Console.WriteLine("{0} {1} {2:mm\\:ss}",
+                    song.Title, song.ArtistName, TimeSpan.FromSeconds(song.Length));
+
+
+            }
+        }
+
     }
 }
