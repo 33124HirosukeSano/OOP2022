bd06d7726d391008ace0ebdc2c7105380dd37d44
33124佐野仁亮 <33124@ojs.ac.jp>
2022-05-12 15:55:29 +0900

    2.3修正

--- Chapter02/Exercise03/Program.cs
+++ Chapter02/Exercise03/Program.cs
@@ -11,27 +11,33 @@ namespace Exercise03 {
             int select = 0;
             var sales = new SalesCounter03("sales.csv");
 
-            Console.WriteLine("売上高の表示選択");
-            Console.WriteLine("1.店舗別");
-            Console.WriteLine("2.商品カテゴリー別");
-            Console.WriteLine(">");
+            while (true) {
 
-            select = int.Parse(Console.ReadLine());
-            IDictionary<string, int> amountPerStore = null;
-            switch (select) {
-                case 1:
-                    amountPerStore = sales.GetParStoreSales();
-                    break;
-                case 2:
-                    amountPerStore = sales.GetParCaregorySales();
-                    break;
+                Console.WriteLine("売上高の表示選択");
+                Console.WriteLine("1.店舗別");
+                Console.WriteLine("2.商品カテゴリー別");
+                Console.WriteLine(">");
+
+                select = int.Parse(Console.ReadLine());
+                IDictionary<string, int> amountPerStore = null;
+                switch (select) {
+                    case 1:
+                        amountPerStore = sales.GetParStoreSales();
+                        break;
+                    case 2:
+                        amountPerStore = sales.GetParCaregorySales();
+                        break;
+                    case 999:
+                        return;
+                }
+
+                foreach (var obj in amountPerStore) {
+
+                    Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+
+                }
             }
 
-            foreach(var obj in amountPerStore) {
-
-                Console.WriteLine("{0} {1}", obj.Key, obj.Value);
-
-            }
 
             /*var sales = new SalesCounter03("sales.csv");
             var amountPerCaregory = sales.GetParCaregorySales();
