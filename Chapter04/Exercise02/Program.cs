c44830e280b25ce72ca6d293075f37e7a8fd9186
33124佐野仁亮 <33124@ojs.ac.jp>
2022-05-24 14:27:12 +0900

    4章2.2完成

--- Chapter04/Exercise02/Program.cs
+++ Chapter04/Exercise02/Program.cs
@@ -8,6 +8,30 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
+            //4.2.1
+
+            var ymCollection = new YearMonth[] {
+
+            new YearMonth(1983, 10),
+            new YearMonth(1984, 11),
+            new YearMonth(1994, 12),
+            new YearMonth(2005, 5),
+            new YearMonth(2011, 10),
+
+        };
+
+            //4.2.2
+            Exercise2_2(ymCollection);
+            Console.WriteLine("------------");
+
+            //4.2.4
+            Exercise2_4(ymCollection);
+            Console.WriteLine("------------");
+
+            //4.2.5
+            Exercise2_5(ymCollection);
+            Console.WriteLine("------------");
+
 
 
             /*var ym = new YearMonth(2001, 8);
@@ -19,6 +43,35 @@ namespace Exercise02 {
 
 
 
+        }
+
+        //4.2.3
+        static YearMonth FindFirst21C(YearMonth[] yms) {
+
+            return null;
+            
+            
+        }
+
+
+        private static void Exercise2_2(YearMonth[] ymCollection) {
+
+            var selected = ymCollection;
+
+            foreach (var ym in selected) {
+
+                Console.WriteLine(ym);
+
+            }
+
+        }
+
+        private static void Exercise2_4(YearMonth[] ymCollection) {
+            
+        }
+
+        private static void Exercise2_5(YearMonth[] ymCollection) {
+            
         }
     }
 }
