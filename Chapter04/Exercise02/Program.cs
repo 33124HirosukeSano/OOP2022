79885dd9a27256abc86c8ba3ce4be820c43f43cd
33124佐野仁亮 <33124@ojs.ac.jp>
2022-05-24 15:58:59 +0900

    4章4.2.5完成

--- Chapter04/Exercise02/Program.cs
+++ Chapter04/Exercise02/Program.cs
@@ -32,6 +32,10 @@ namespace Exercise02 {
             Exercise2_5(ymCollection);
             Console.WriteLine("------------");
 
+            //4.2.6
+            Exercise2_6(ymCollection);
+            Console.WriteLine("------------");
+
 
 
             /*var ym = new YearMonth(2001, 8);
@@ -45,20 +49,39 @@ namespace Exercise02 {
 
         }
 
+        private static void Exercise2_6(YearMonth[] ymCollection) {
+
+            foreach (var ym in ymCollection.OrderByDescending(ym => ym.Year)) {
+
+                Console.WriteLine(ym);
+
+            }
+
+        }
+
         //4.2.3
         static YearMonth FindFirst21C(YearMonth[] yms) {
 
+
+
+            foreach (var ym in yms ) {
+
+                if (ym.Is21Century) {
+
+                    return ym;
+
+                }
+
+            }  
+
             return null;
-            
             
         }
 
 
-        private static void Exercise2_2(YearMonth[] ymCollection) {
+        private static void Exercise2_2(YearMonth[] ymCollection) {            
 
-            var selected = ymCollection;
-
-            foreach (var ym in selected) {
+            foreach (var ym in ymCollection) {
 
                 Console.WriteLine(ym);
 
@@ -67,11 +90,31 @@ namespace Exercise02 {
         }
 
         private static void Exercise2_4(YearMonth[] ymCollection) {
-            
+
+            var yearmonth = FindFirst21C(ymCollection);
+
+            if (yearmonth != null) {
+
+                Console.WriteLine(yearmonth);
+
+            } else {
+
+                Console.WriteLine("２１世紀のデータはありません");
+
+            }
+
         }
 
         private static void Exercise2_5(YearMonth[] ymCollection) {
-            
+
+            var array = ymCollection.Select(ym => ym.AddOneMonth());
+            foreach (var ym in array) {
+
+                Console.WriteLine(ym);
+
+            }
+
+
         }
     }
 }
