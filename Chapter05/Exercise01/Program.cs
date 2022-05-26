9143a248aa7f618ae198527c1aed28870bfdaee7
33124佐野仁亮 <33124@ojs.ac.jp>
2022-05-26 15:16:36 +0900

    5章5.1完成

--- Chapter05/Exercise01/Program.cs
+++ Chapter05/Exercise01/Program.cs
@@ -7,6 +7,24 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     class Program {
         static void Main(string[] args) {
+
+            Console.WriteLine("文字を入力");
+            Console.Write("1つ目：");
+            var line1 = Console.ReadLine();
+            Console.Write("2つ目：");
+            var line2 = Console.ReadLine();
+
+            if (String.Compare(line1, line2, true) == 0) {
+
+                Console.WriteLine("等しい");
+
+            } else {
+
+                Console.WriteLine("等しくない");
+
+            }
+
+
         }
     }
 }
