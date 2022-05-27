47ea2f685dd78c3750d1079f35d8d6c3d6e00939
33124佐野仁亮 <33124@ojs.ac.jp>
2022-05-27 13:36:31 +0900

    5章5.2完成

---
+++ Chapter05/Exercise02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+
+            if (int.TryParse(Console.ReadLine(), out int intline1)) {               
+                
+                Console.WriteLine("{0:#,#}",intline1);
+
+            } else {
+
+                Console.WriteLine("変換できません");
+
+            }
+
+        }
+    }
+}
