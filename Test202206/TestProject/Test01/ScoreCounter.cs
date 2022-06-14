bebe7d3c09630aa722960a3fd28fc8361dde7b4b
33124佐野仁亮 <33124@ojs.ac.jp>
2022-06-14 14:29:45 +0900

    途中経過

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -8,32 +8,48 @@ namespace Test01 {
         // コンストラクタ
         public ScoreCounter(string filePath) {
 
+            _score = ReadScore(filePath);
 
-
-            
         }
 
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
+            var dict = new Dictionary<string, int>();
+            foreach (var score in _score) {
+
+                if (dict.ContainsKey(score.Name))
+                    dict[score.Name] += score.Score;
+                else
+                    dict[score.Name] = score.Score;
+
+            }
+
+            return dict;
 
 
-
-
-
-
-            
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
+            List<Student> sales = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                core sale = new Sale {
+
+                    ShopName = items[0],
+                    ProductCaregory = items[1],
+                    Amount = int.Parse(items[2])
+
+                };
+                sales.Add(sale);
+            }
 
 
 
+            return sales;
 
-
-            
         }
     }
 }
