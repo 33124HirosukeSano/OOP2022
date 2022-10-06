6e4381da69769f8c25305c1b017d519535008741
33124佐野仁亮 <33124@ojs.ac.jp>
2022-10-06 09:30:45 +0900

    10月6日1回目

--- Chapter15/Chapter15/Program.cs
+++ Chapter15/Chapter15/Program.cs
@@ -8,27 +8,56 @@ namespace Chapter15 {
     class Program {
         static void Main(string[] args) {
 
+            var years = new List<int>();
+            int s;
+            int sort;
             
 
             Console.WriteLine("出力したい西暦を入力(終了-1)");
-            int s =  int.Parse(Console.ReadLine());
+            s =  int.Parse(Console.ReadLine());
 
-            if (s == -1) {
-                var years = new int[] { s };
-                var books = Library.Books
-                                   .Where(b => years.Contains(b.PublishedYear));
+            while (s != -1) {
 
-                foreach (var book in books) {
+                years.Add(s);
+                s = int.Parse(Console.ReadLine());
 
-                    Console.WriteLine(book);
+            }
 
-                }
+            Console.WriteLine("昇順：1　降順：2");
+            sort = int.Parse(Console.ReadLine());
+
+            IEnumerable<Book> books;
+            if (sort == 1) {
+
+                books = Library.Books
+                               .Where(b => years.Contains(b.PublishedYear))
+                               .OrderBy(b => b.PublishedYear);
 
             } else {
 
+                books = Library.Books
+                               .Where(b => years.Contains(b.PublishedYear))
+                               .OrderByDescending(b => b.PublishedYear);
 
+            }
+            
+            
 
-            }            
+            foreach (var book in books) {
+
+                Console.WriteLine(book);
+
+            }
+
+            Console.WriteLine();
+            var groups = Library.Books
+                .Where(b => years.Contains(b.PublishedYear))
+                .GroupBy(b => b.PublishedYear)
+                .OrderBy(g => g.Key);
+            foreach (var g in groups) {
+
+            }
+
         }
     }
 }
