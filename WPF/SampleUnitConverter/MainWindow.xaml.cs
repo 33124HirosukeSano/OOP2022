38988ed39531ef640e0e604394ddaddd34312b03
33124佐野仁亮 <33124@ojs.ac.jp>
2022-10-20 15:58:00 +0900

    10月20日　午後

--- WPF/SampleUnitConverter/MainWindow.xaml.cs
+++ WPF/SampleUnitConverter/MainWindow.xaml.cs
@@ -18,8 +18,12 @@ namespace SampleUnitConverter {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+
         public MainWindow() {
+
             InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
+
         }
     }
 }
