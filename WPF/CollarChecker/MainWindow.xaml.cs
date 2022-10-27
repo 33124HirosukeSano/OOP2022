681d5ef2a9176b2e4318dc6bbcd1ff92e4941d4f
33124佐野仁亮 <33124@ojs.ac.jp>
2022-10-27 10:37:54 +0900

    10月27日　前半

--- WPF/CollarChecker/MainWindow.xaml.cs
+++ WPF/CollarChecker/MainWindow.xaml.cs
@@ -18,14 +18,49 @@ namespace CollarChecker {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
-        public double Rpoint = 0;
-
+        
         public MainWindow() {
             InitializeComponent();
         }
 
-        private void RedSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
 
+            var doubleRpoint = Double.Parse(rPoint.Text);
+            var doubleGpoint = Double.Parse(gPoint.Text);
+            var doubleBpoint = Double.Parse(bPoint.Text);
+            
+
+            var nrPoint = byte.Parse(Math.Truncate(doubleRpoint).ToString());
+            var ngPoint = byte.Parse(Math.Truncate(doubleGpoint).ToString());
+            var nbPoint = byte.Parse(Math.Truncate(doubleBpoint).ToString());
+
+            SolidColorBrush brush = new SolidColorBrush(Color.FromRgb(nrPoint, ngPoint, nbPoint));
+
+            cLabel.Background = brush;
+
+        }
+
+        private void Slider_ValueChanged(object sender, TextChangedEventArgs e) {
+
+            /*var doubleRpoint = Double.Parse(rPoint.Text);
+            var doubleGpoint = Double.Parse(gPoint.Text);
+            var doubleBpoint = Double.Parse(bPoint.Text);
+
+            var truncateRpoint = Math.Truncate(doubleRpoint).ToString();
+            var truncateGpoint = Math.Truncate(doubleGpoint).ToString();
+            var truncateBpoint = Math.Truncate(doubleBpoint).ToString();
+
+            var nrPoint = byte.Parse(truncateRpoint);
+            var ngPoint = byte.Parse(truncateGpoint);
+            var nbPoint = byte.Parse(truncateBpoint);
+
+            SolidColorBrush brush = new SolidColorBrush(Color.FromRgb(nrPoint, ngPoint, nbPoint));
+
+            cLabel.Background = brush;*/
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
             
 
         }
