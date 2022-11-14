3a7f2e19128129a7a927e9a2bcbd8c7b4f3cbb8c
33124佐野仁亮 <33124@ojs.ac.jp>
2022-11-14 15:57:01 +0900

    地域別に2つのコンボボックスを連動

---
+++ Chapter14/WeatherApp/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace WeatherApp {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.tbWeatherInfo = new System.Windows.Forms.TextBox();
+            this.btWeatherGet = new System.Windows.Forms.Button();
+            this.cbPrefecture = new System.Windows.Forms.ComboBox();
+            this.cbRegion = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbWeatherInfo
+            // 
+            this.tbWeatherInfo.BackColor = System.Drawing.SystemColors.Window;
+            this.tbWeatherInfo.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbWeatherInfo.Location = new System.Drawing.Point(119, 413);
+            this.tbWeatherInfo.Multiline = true;
+            this.tbWeatherInfo.Name = "tbWeatherInfo";
+            this.tbWeatherInfo.ReadOnly = true;
+            this.tbWeatherInfo.Size = new System.Drawing.Size(572, 123);
+            this.tbWeatherInfo.TabIndex = 0;
+            // 
+            // btWeatherGet
+            // 
+            this.btWeatherGet.Location = new System.Drawing.Point(722, 505);
+            this.btWeatherGet.Name = "btWeatherGet";
+            this.btWeatherGet.Size = new System.Drawing.Size(90, 31);
+            this.btWeatherGet.TabIndex = 1;
+            this.btWeatherGet.Text = "取得";
+            this.btWeatherGet.UseVisualStyleBackColor = true;
+            this.btWeatherGet.Click += new System.EventHandler(this.btWeatherGet_Click);
+            // 
+            // cbPrefecture
+            // 
+            this.cbPrefecture.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cbPrefecture.FormattingEnabled = true;
+            this.cbPrefecture.Items.AddRange(new object[] {
+            "宗谷地方",
+            "上川・留萌地方",
+            "網走・北見・紋別地方",
+            "十勝地方",
+            "釧路・根室地方",
+            "胆振・日高地方",
+            "石狩・空知・後志地方",
+            "渡島・檜山地方"});
+            this.cbPrefecture.Location = new System.Drawing.Point(180, 165);
+            this.cbPrefecture.Name = "cbPrefecture";
+            this.cbPrefecture.Size = new System.Drawing.Size(347, 35);
+            this.cbPrefecture.TabIndex = 2;
+            // 
+            // cbRegion
+            // 
+            this.cbRegion.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cbRegion.FormattingEnabled = true;
+            this.cbRegion.Location = new System.Drawing.Point(180, 72);
+            this.cbRegion.Name = "cbRegion";
+            this.cbRegion.Size = new System.Drawing.Size(347, 35);
+            this.cbRegion.TabIndex = 3;
+            this.cbRegion.TextChanged += new System.EventHandler(this.cbRegion_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(17, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "地方";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(18, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "都道府県(地域)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 604);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbRegion);
+            this.Controls.Add(this.cbPrefecture);
+            this.Controls.Add(this.btWeatherGet);
+            this.Controls.Add(this.tbWeatherInfo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbWeatherInfo;
+        private System.Windows.Forms.Button btWeatherGet;
+        private System.Windows.Forms.ComboBox cbPrefecture;
+        private System.Windows.Forms.ComboBox cbRegion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Chapter14/WeatherApp/Form1.cs
@@ -0,0 +1,298 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherApp {
+    public partial class Form1 : Form {
+        string dString;
+        private DataTable dataTable;
+
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+
+
+        private void btWeatherGet_Click(object sender, EventArgs e) {
+
+            var wc = new WebClient() {
+
+                Encoding = Encoding.UTF8
+
+            };
+
+            switch (cbPrefecture.Text) {
+                case "宗谷":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/011000.json");
+                    break;
+                case "上川・留萌":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/012000.json");
+                    break;
+                case "網走・北見・紋別":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/013000.json");
+                    break;
+                case "十勝":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/014030.json");
+                    break;
+                case "釧路・根室":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/014100.json");
+                    break;
+                case "胆振・日高":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/015000.json");
+                    break;
+                case "石狩・空知・後志":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/016000.json");
+                    break;
+                case "渡島・檜山":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/017000.json");
+                    break;
+                case "青森県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/020000.json");
+                    break;
+                case "岩手県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/030000.json");
+                    break;
+                case "宮城県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/040000.json");
+                    break;
+                case "秋田県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/050000.json");
+                    break;
+                case "山形県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/060000.json");
+                    break;
+                case "福島県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/070000.json");
+                    break;
+                case "茨城県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/080000.json");
+                    break;
+                case "栃木県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/090000.json");
+                    break;
+                case "群馬県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/100000.json");
+                    break;
+                case "埼玉県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/110000.json");
+                    break;
+                case "千葉県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/120000.json");
+                    break;
+                case "東京都":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/130000.json");
+                    break;
+                case "神奈川県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/140000.json");
+                    break;
+                case "新潟県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/150000.json");
+                    break;
+                case "富山県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/160000.json");
+                    break;
+                case "石川県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/170000.json");
+                    break;
+                case "福井県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/180000.json");
+                    break;
+                case "山梨県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/190000.json");
+                    break;
+                case "長野県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/200000.json");
+                    break;
+                case "岐阜県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/210000.json");
+                    break;
+                case "静岡県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/220000.json");
+                    break;
+                case "愛知県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/230000.json");
+                    break;
+                case "三重県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/240000.json");
+                    break;
+                case "滋賀県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/250000.json");
+                    break;
+                case "京都府":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/260000.json");
+                    break;
+                case "大阪府":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/270000.json");
+                    break;
+                case "兵庫県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/280000.json");
+                    break;
+                case "奈良県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/290000.json");
+                    break;
+                case "和歌山県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/300000.json");
+                    break;
+                case "鳥取県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/310000.json");
+                    break;
+                case "島根県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/320000.json");
+                    break;
+                case "岡山県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/330000.json");
+                    break;
+                case "広島県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/340000.json");
+                    break;
+                case "山口県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/350000.json");
+                    break;
+                case "徳島県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/360000.json");
+                    break;
+                case "香川県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/370000.json");
+                    break;
+                case "愛媛県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/380000.json");
+                    break;
+                case "高知県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/390000.json");
+                    break;
+                case "福岡県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/400000.json");
+                    break;
+                case "佐賀県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/410000.json");
+                    break;
+                case "長崎県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/420000.json");
+                    break;
+                case "熊本県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/430000.json");
+                    break;
+                case "大分県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/440000.json");
+                    break;
+                case "宮崎県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/450000.json");
+                    break;
+                case "鹿児島県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/460100.json");
+                    break;
+                case "沖縄県":
+                    dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/471000.json");
+                    break;
+                default:
+                    break;
+            }
+
+            var dString2 = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/forecast/100000.json");
+
+            var json = JsonConvert.DeserializeObject<Rootobject>(dString);
+            var json2 = JsonConvert.DeserializeObject<Class1[]>(dString2);
+            tbWeatherInfo.Text = json2[0].timeSeries[0].areas[0].weathers[1];
+            //tbWeatherInfo.Text = json.text;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+            dataTable = new DataTable("Region");
+
+            dataTable.Columns.Add("region", typeof(string));
+            dataTable.Columns.Add("prefecture", typeof(string));
+
+            dataTable.Rows.Add("北海道地方", "宗谷");
+            dataTable.Rows.Add("北海道地方", "上川・留萌");
+            dataTable.Rows.Add("北海道地方", "網走・北見・紋別");
+            dataTable.Rows.Add("北海道地方", "十勝");
+            dataTable.Rows.Add("北海道地方", "釧路・根室");
+            dataTable.Rows.Add("北海道地方", "胆振・日高");
+            dataTable.Rows.Add("北海道地方", "石狩・空知・後志");
+            dataTable.Rows.Add("北海道地方", "渡島・檜山");
+            dataTable.Rows.Add("東北地方", "青森県");
+            dataTable.Rows.Add("東北地方", "岩手県");
+            dataTable.Rows.Add("東北地方", "宮城県");
+            dataTable.Rows.Add("東北地方", "秋田県");
+            dataTable.Rows.Add("東北地方", "山形県");
+            dataTable.Rows.Add("東北地方", "福島県");
+            dataTable.Rows.Add("関東地方", "茨城県");
+            dataTable.Rows.Add("関東地方", "栃木県");
+            dataTable.Rows.Add("関東地方", "群馬県");
+            dataTable.Rows.Add("関東地方", "埼玉県");
+            dataTable.Rows.Add("関東地方", "千葉県");
+            dataTable.Rows.Add("関東地方", "東京都");
+            dataTable.Rows.Add("関東地方", "神奈川県");
+            dataTable.Rows.Add("中部地方", "新潟県");
+            dataTable.Rows.Add("中部地方", "富山県");
+            dataTable.Rows.Add("中部地方", "石川県");
+            dataTable.Rows.Add("中部地方", "福井県");
+            dataTable.Rows.Add("中部地方", "山梨県");
+            dataTable.Rows.Add("中部地方", "長野県");
+            dataTable.Rows.Add("中部地方", "岐阜県");
+            dataTable.Rows.Add("中部地方", "静岡県");
+            dataTable.Rows.Add("中部地方", "愛知県");
+            dataTable.Rows.Add("近畿地方", "三重県");
+            dataTable.Rows.Add("近畿地方", "滋賀県");
+            dataTable.Rows.Add("近畿地方", "京都府");
+            dataTable.Rows.Add("近畿地方", "大阪府");
+            dataTable.Rows.Add("近畿地方", "兵庫県");
+            dataTable.Rows.Add("近畿地方", "奈良県");
+            dataTable.Rows.Add("近畿地方", "和歌山県");
+            dataTable.Rows.Add("中国地方", "鳥取県");
+            dataTable.Rows.Add("中国地方", "島根県");
+            dataTable.Rows.Add("中国地方", "岡山県");
+            dataTable.Rows.Add("中国地方", "広島県");
+            dataTable.Rows.Add("中国地方", "山口県");
+            dataTable.Rows.Add("四国地方", "徳島県");
+            dataTable.Rows.Add("四国地方", "香川県");
+            dataTable.Rows.Add("四国地方", "愛媛県");
+            dataTable.Rows.Add("四国地方", "高知県");
+            dataTable.Rows.Add("九州地方", "福岡県");
+            dataTable.Rows.Add("九州地方", "佐賀県");
+            dataTable.Rows.Add("九州地方", "長崎県");
+            dataTable.Rows.Add("九州地方", "熊本県");
+            dataTable.Rows.Add("九州地方", "大分県");
+            dataTable.Rows.Add("九州地方", "宮崎県");
+            dataTable.Rows.Add("九州地方", "鹿児島県");
+            dataTable.Rows.Add("九州地方", "沖縄県");
+
+            var resultDataTableForArea = dataTable.DefaultView.ToTable(true, new string[] { "region" });
+            this.cbRegion.DisplayMember = "region";
+            this.cbRegion.ValueMember = "region";
+            this.cbRegion.DataSource = resultDataTableForArea;
+            this.cbRegion.SelectedIndex = 0;
+
+            this.InitializePrefectureComboBox();
+
+        }
+        private void InitializePrefectureComboBox() {
+            var resultDataTableForPrefecture = dataTable.AsEnumerable().Where(
+              row => row.Field<string>("region") == this.cbRegion.SelectedValue.ToString())
+              .AsDataView().ToTable(true, new string[] { "region", "prefecture" });
+            this.cbPrefecture.DisplayMember = "prefecture";
+            this.cbPrefecture.ValueMember = "prefecture";
+            this.cbPrefecture.DataSource = resultDataTableForPrefecture;
+            this.cbPrefecture.SelectedIndex = 0;
+        }
+
+
+        private void cbRegion_TextChanged(object sender, EventArgs e) {
+
+            this.InitializePrefectureComboBox();
+
+        }
+ 
+    }
+}
